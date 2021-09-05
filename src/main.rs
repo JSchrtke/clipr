d6a4dd2c4556797502824548263d9817e3743d32
JSchrtke <jschrtke@protonmail.com>
2021-09-05 20:06:42 +0200

    Add basic clipboard handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use atty::{isnt, Stream};
+use copypasta::ClipboardProvider;
+use copypasta_ext::x11_bin::ClipboardContext;
+use std::io::{self, Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut clip_ctx = ClipboardContext::new().unwrap();
+
+    // If stdin is a pipe, read all available data and set the clipboards content to it
+    if isnt(Stream::Stdin) {
+        let mut buf = String::new();
+        let mut stdin = io::stdin();
+
+        match stdin.read_to_string(&mut buf) {
+            Ok(_) => (),
+            Err(err) => {
+                eprintln!("Error reading from stdin: {}", err);
+                return;
+            }
+        };
+
+        clip_ctx.set_contents(buf.to_owned()).unwrap();
+    }
+
+    // If stdout is a pipe, write the clipboard content to it
+    if isnt(Stream::Stdout) {
+        let mut stdout = io::stdout();
+
+        let content: String = match clip_ctx.get_contents() {
+            Ok(str) => str,
+            Err(err) => {
+                eprintln!("Error getting clipboard contents: {}", err);
+                return;
+            }
+        };
+
+        match stdout.write_all(content.as_bytes()) {
+            Ok(_) => (),
+            Err(err) => {
+                eprintln!("Error writing to stdout: {}", err);
+                return;
+            }
+        };
+    }
 }
